40b4f23ef2bd56b60744a572fbba65c7791e8455
David Weiss <mail@dweiss.eu>
2018-08-13 15:19:15 +0200

    update

--- src/main/java/deutschebank/dbutils/DBConnector.java
+++ src/main/java/deutschebank/dbutils/DBConnector.java
@@ -64,6 +64,8 @@ public class DBConnector
             itsConnection = DriverManager.getConnection(dbPath + dbName, 
                                                     dbUser, 
                                                     dbPwd );
+            
+            
 
 
             /*
